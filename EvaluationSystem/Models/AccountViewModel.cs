c0bdc7cd31b4b579e1aa826dac8f919b267f0796
letanhy <Letanhy130401@gmail.com>
2024-06-21 15:08:46 +0700

    Login

---
+++ EvaluationSystem/Models/AccountViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EvaluationSystem.Models
+{
+    public class AccountViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập tài khoản.")]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
